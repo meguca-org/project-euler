50f3c965b967e88f8d573f6ab3c9dbb9866ee940
iaqidn <nihilisticentity@gmail.com>
2020-06-07 19:29:07 -0400

    Problem 1 fix
    10000 -> 1000
    tehe pero

--- Problem 001/iaqidn.c
+++ Problem 001/iaqidn.c
@@ -3,7 +3,7 @@
 int main() {
     int sum = 0;
 
-    for (int i = 3; i < 10000; i++)
+    for (int i = 3; i < 1000; i++)
         if (i % 3 == 0 || i % 5 == 0)
             sum += i;
 
--- Problem 001/iaqidn.py
+++ Problem 001/iaqidn.py
@@ -1,5 +1,5 @@
 sum = 0
-for n in range(3, 10000):
+for n in range(3, 1000):
     sum += n if n % 3 == 0 or n % 5 == 0 else 0
 
 print(sum)
