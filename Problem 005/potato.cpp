255f583cda459cbf26329fa01361952cb0655c74
literally-a-potato <potato@bernkastel.dev>
2020-06-07 17:51:00 -0700

    potato-5

---
+++ Problem 005/potato.cpp
@@ -0,0 +1,8 @@
+#include <boost/integer/common_factor.hpp>
+#include <cstdio>
+
+int main()
+{
+	printf("%d\n", boost::integer::lcm(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
+	return 0;
+}
